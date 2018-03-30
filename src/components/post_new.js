8c7253cb8bc3a1c1f2aabc226ec6ce0caa5a438d
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-03-29 17:52:46 -0700

    add redux-form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
+    renderField(field){
+        return (
+            <div className = 'form-group'>
+                <label> {field.label}</label>
+                <input
+                    className = 'form-control'
+                    type = 'text'
+                    {...field.input}
+                />
+            </div>
+        )
+    }
     render(){
         return(
             <div>
-                POSTNEW
+                <form>
+                    <Field  label = 'Title' name = 'title' component={this.renderField}  />
+                    <Field label = 'tags' name = 'tags' component={this.renderField}  />
+                    <Field label = 'contents' name = 'contents' component={this.renderField}  />
+                </form>
             </div>
         )
     }
 }
 
-export default PostsNew
+export default reduxForm({ form: 'PostsNewForm' })(PostsNew)
